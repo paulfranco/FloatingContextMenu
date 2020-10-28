e3ad5cae70507063b6656d7e5b36f7713fdd63bf
Paul Franco <mail@paulfran.co>
2020-10-28 02:54:19 -0700

    Initial commit

---
+++ app/src/main/java/co/paulfran/floatingcontextmenu/MainActivity.kt
@@ -0,0 +1,38 @@
+package co.paulfran.floatingcontextmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Register Button For our Menu
+        registerForContextMenu(openMenuButton)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.floating_context_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.item1 -> Toast.makeText(this, "Item 1 Clicked", Toast.LENGTH_SHORT).show()
+            R.id.item2 -> Toast.makeText(this, "Item 2 Clicked", Toast.LENGTH_SHORT).show()
+            R.id.item3 -> Toast.makeText(this, "Item 3 Clicked", Toast.LENGTH_SHORT).show()
+            R.id.item4 -> Toast.makeText(this, "Item 4 Clicked", Toast.LENGTH_SHORT).show()
+        }
+        return super.onContextItemSelected(item)
+    }
+}
